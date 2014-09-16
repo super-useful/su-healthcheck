c4884d70a0b818407af86f28f14cc7ef18dd545c
constantology <constantology@gmail.com>
2014-09-16 14:21:39 +0100

    changing from bash command calls to using npm modules so we're not bound by needing native clients on boxes we run health check on

--- example.js
+++ example.js
@@ -1,3 +1,5 @@
+var co = require('co');
+
 var df = require('./lib/df');
 var memory = require('./lib/memory');
 var redis = require('./lib/redis');
@@ -5,12 +7,15 @@ var mongo = require('./lib/mongodb');
 var postgres = require('./lib/postgres');
 var ssh_tunnel = require('./lib/ssh_tunnel');
 
-console.log({
-    available_diskspace : df(),
-    free_memory : memory(),
-    node : process.version,
-    redis : redis('127.0.0.1', 6379),
-    mongodb : mongo('127.0.0.1', 27017, 'crunchgoat', ['reads']),
-    postgres : postgres('127.0.0.1', 5432, 'ubuntu', 'ubuntu', 'me', ['contract', 'data_range', 'reads']),
-    postgres_tunnel : ssh_tunnel('54.195.60.13', 5432, '127.0.0.1', 5432, 'ubuntu')
-});
+co(function* () {
+    console.log({
+        available_diskspace : df(),
+        free_memory : memory(),
+        node : process.version,
+        redis : yield redis('127.0.0.1', 6379),
+        crunchgoat : yield mongo('127.0.0.1', 27017, 'crunchgoat', ['reads']),
+        goatheist : yield mongo('127.0.0.1', 27017, 'goatheist', ['users', 'breakdown', 'comparison']),
+        postgres : yield postgres('127.0.0.1', 5432, 'ubuntu', 'ubuntu', 'me'),
+        postgres_tunnel : ssh_tunnel('54.195.60.13', 5432, '127.0.0.1', 5432, 'ubuntu')
+    });
+})();
