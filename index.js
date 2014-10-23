81f0c81790581cadd1a7b1b3b09aabb045c21af3
constantology <constantology@gmail.com>
2014-10-23 10:51:19 +0100

    OCD: inlined require statements

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-var df = require('./lib/df');
-var memory = require('./lib/memory');
-var redis = require('./lib/redis');
-var mongo = require('./lib/mongodb');
-var postgres = require('./lib/postgres');
-var sshTunnel = require('./lib/ssh_tunnel');
-var verify = require('./lib/verify');
-
-
 module.exports = {
-    df: df,
-    version: function () {return process.version},
-    memory: memory,
-    redis: redis,
-    mongo: mongo,
-    postgres: postgres,
-    sshTunnel: sshTunnel,
-    verify: verify
+    df : require('./lib/df'),
+    memory : require('./lib/memory'),
+    redis : require('./lib/redis'),
+    mongo : require('./lib/mongodb'),
+    postgres : require('./lib/postgres'),
+    sshTunnel : require('./lib/ssh_tunnel'),
+    verify : require('./lib/verify'),
+    version : function() {
+        return process.version;
+    }
 };
